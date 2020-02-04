b447d4666e01fb3a873e8bca37b773d2e3a890ab
Mike Decker <pookmish@gmail.com>
2020-02-04 13:19:35 -0800

    D8CORE-1221 Darker tool labels

--- js/src/Components/Paragraph.js
+++ js/src/Components/Paragraph.js
@@ -107,7 +107,7 @@ export const Paragraph = ({item, ...props}) => {
                     justifyContents="left"
                     alignItems="center"
                     style={{
-                      color: "#9e9e9e",
+                      color: "#4d4f53",
                       border: "1px solid #BDBDBD",
                       padding: "20px 50px",
                       lineHeight: "35px",
