f4a1f3db4039c710ce5abd7889eeefccdd639558
Mike Decker <pookmish@gmail.com>
2020-03-17 19:19:57 -0700

    Fix tab indexes when the media browser is open

--- js/src/Components/Atoms/FormDialog.js
+++ js/src/Components/Atoms/FormDialog.js
@@ -3,9 +3,11 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Fade from '@material-ui/core/Fade';
 
 export const FormDialog = ({open, title, formContent, onClose, ...props}) => {
   let submitButton = React.createRef();
+  let dialogRef = React.createRef();
 
   /**
    * Form submit handler.
@@ -19,6 +21,12 @@ export const FormDialog = ({open, title, formContent, onClose, ...props}) => {
     submitButton.current.click();
   };
 
+  const onEntered = () => {
+    // Remove the tab index because it breaks when there is a tab index and
+    // the media browser is open.
+    dialogRef.current.removeAttribute('tabindex');
+  };
+
   return (
     <Dialog
       maxWidth='lg'
@@ -26,10 +34,9 @@ export const FormDialog = ({open, title, formContent, onClose, ...props}) => {
       aria-labelledby="max-width-dialog-title"
       onBackdropClick={onBackdropClick}
       onEscapeKeyDown={onBackdropClick}
-      style={{zIndex:'1100'}}
-      PaperProps={{
-        style: {zIndex: 1}
-      }}
+      style={{zIndex: 900}}
+      onEntered={onEntered}
+      TransitionProps={{ref: dialogRef}}
     >
       <DialogTitle style={{
         background: '#6b6b6b',
