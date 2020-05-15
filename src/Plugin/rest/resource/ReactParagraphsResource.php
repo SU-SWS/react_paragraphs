47cdac9cd8eb70a69cd31d0d8f8c87c04c17f8e1
Mike Decker <pookmish@gmail.com>
2020-05-15 08:48:28 -0700

    Use alter method and fixed the object in the react field

--- src/Plugin/rest/resource/ReactParagraphsResource.php
+++ src/Plugin/rest/resource/ReactParagraphsResource.php
@@ -129,13 +129,11 @@ class ReactParagraphsResource extends ResourceBase {
       $field_config = $field_config_storage->load("$entity_type_id.$bundle.$field_name");
       if ($field_config && ($plugin = $this->getReactFieldsPlugin($field_config))) {
         $data[$field_name] = $plugin->getFieldInfo($form[$field_name], $field_config);
-        $this->moduleHandler->invokeAll(
-          "react_paragraphs_getfieldinfo_post_alter",
-          [
-            $form[$field_name],
-            $field_config,
-            $data[$field_name],
-          ]
+        $this->moduleHandler->alter(
+          'react_paragraphs_form_field_data',
+          $data[$field_name],
+          $form[$field_name],
+          $field_config
         );
       }
     }
