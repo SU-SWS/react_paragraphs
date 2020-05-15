5033d6995021493a3f50b68a6999fe747d0906f0
Shea McKinney <sherakama@gmail.com>
2020-05-15 07:22:34 -0700

    fixup! check for altered.

--- src/Plugin/rest/resource/ReactParagraphsResource.php
+++ src/Plugin/rest/resource/ReactParagraphsResource.php
@@ -129,7 +129,7 @@ class ReactParagraphsResource extends ResourceBase {
       $field_config = $field_config_storage->load("$entity_type_id.$bundle.$field_name");
       if ($field_config && ($plugin = $this->getReactFieldsPlugin($field_config))) {
         $data[$field_name] = $plugin->getFieldInfo($form[$field_name], $field_config);
-        $data[$field_name] = $this->moduleHandler->invokeAll(
+        $altered = $this->moduleHandler->invokeAll(
           "react_paragraphs_getfieldinfo_post_alter",
           [
             $form[$field_name],
@@ -137,6 +137,9 @@ class ReactParagraphsResource extends ResourceBase {
             $data[$field_name],
           ]
         );
+        if ($altered) {
+          $data[$field_name] = $altered;
+        }
       }
     }
 
