f6ed5333573b869e854551f1582277528193e0b4
Mike Decker <pookmish@gmail.com>
2020-09-30 16:03:22 -0700

    fixup! Prevent circular dependencies

--- modules/react_paragraphs_behaviors/src/Config/ConfigOverrides.php
+++ modules/react_paragraphs_behaviors/src/Config/ConfigOverrides.php
@@ -5,7 +5,6 @@ namespace Drupal\react_paragraphs_behaviors\Config;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryOverrideInterface;
 use Drupal\Core\Config\StorageInterface;
-use Drupal\react_paragraphs_behaviors\ReactBehaviorsPluginManagerInterface;
 
 /**
  * Class ConfigOverrides.
