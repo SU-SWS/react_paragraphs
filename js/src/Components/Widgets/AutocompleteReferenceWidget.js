40afd361d8ffc73540be6da0492f5eb4b8e09872
Mike Decker <pookmish@gmail.com>
2020-10-05 14:56:10 -0700

    Working entity reference field for a single item

--- js/src/Components/Widgets/AutocompleteReferenceWidget.js
+++ js/src/Components/Widgets/AutocompleteReferenceWidget.js
@@ -1,21 +1,49 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {FormHelperText} from "@material-ui/core";
 import TextField from '@material-ui/core/TextField';
 import FormGroup from '@material-ui/core/FormGroup';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 export const AutocompleteReferenceWidget = ({defaultValue, fieldId, fieldName, onFieldChange, settings}) => {
-  console.log(settings);
+
+  const options = settings.options.map((option) => {
+    const firstLetter = option.label[0].toUpperCase();
+    return {
+      firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+      ...option,
+    };
+  });
+
+  const getValue = () => {
+    if (defaultValue.length > 0) {
+      return options.find(option => option.entityId === defaultValue[0].target_id);
+    }
+  }
+
+  const onChange = (e, newValue) => {
+    if (newValue) {
+      return onFieldChange([{target_id: newValue.entityId}])
+    }
+
+    onFieldChange([]);
+  }
 
   return (
-    <Autocomplete
-      id="combo-box-demo"
-      options={settings.options}
-      getOptionLabel={(option) => option.label}
-      style={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-    />
+    <FormGroup>
+      <Autocomplete
+        id={fieldId}
+        multiple={false}
+        options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+        getOptionLabel={(option) => option.label + ' (' + option.entityId + ')'}
+        groupBy={(option) => option.firstLetter}
+        onChange={onChange}
+        value={getValue()}
+        renderInput={(params) => (
+          <TextField {...params} label={settings.label} variant="outlined"/>
+        )}
+      />
+      {settings.help.length > 0 &&
+      <FormHelperText>{settings.help}</FormHelperText>}
+    </FormGroup>
   )
 }
