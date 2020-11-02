f9a7793c8b113dc7c2cd2374f48e1d32fb2d4325
pookmish <pookmish@stanford.edu>
2020-11-02 10:01:58 -0800

    Removed unused import

--- js/src/Components/Atoms/RowActions.js
+++ js/src/Components/Atoms/RowActions.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { withStyles } from '@material-ui/core/styles';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import {WidgetContext} from "../../Contexts/WidgetManager";
