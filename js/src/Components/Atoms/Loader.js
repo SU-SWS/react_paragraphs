785d5cc0de88ac87d0446f11f2928dc8d7b4ad74
Mike Decker <pookmish@gmail.com>
2021-01-19 13:44:09 -0800

    wrap in an error boundary to handle errors

--- js/src/Components/Atoms/Loader.js
+++ js/src/Components/Atoms/Loader.js
@@ -3,7 +3,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 export const Loader = () => {
   return (
-    <div style={{position: 'absolute', left: '50%', top: '50%'}}>
+    <div style={{position: 'absolute', left: 'calc(50% - 20px)', top: 'calc(50% - 20px)'}}>
       <CircularProgress/>
     </div>
   )
--- js/src/Components/Widgets/Media/MediaList.js
+++ js/src/Components/Widgets/Media/MediaList.js
@@ -3,21 +3,27 @@ import styled from 'styled-components';
 import {FlexDiv} from "../../Atoms/FlexDiv";
 import {XButton} from "../../Atoms/XButton";
 import {Loader} from "../../Atoms/Loader";
+import {ErrorBoundary} from "../../Atoms/ErrorBoundary";
 
 const PreviewContainer = styled.div`
   border: 1px solid #dbdbdb;
   position: relative;
   max-width: 200px;
   margin: 0 10px 10px 0;
+  min-height: 100px;
+  min-width: 100px;
 `;
 
-const PreviewImage = styled.img`
+const PreviewImageTag = styled.img`
   height: 180px;
   object-fit: contain;
   object-position: center center;
   max-width: 100%;
 `;
 
+/**
+ * List of media items for media widget.
+ */
 export const MediaList = ({selectedItems, onRemove, updateOrder}) => {
 
   return (
@@ -36,24 +42,29 @@ export const MediaList = ({selectedItems, onRemove, updateOrder}) => {
   )
 }
 
+/**
+ * Individual media item.
+ */
 const MediaItem = ({mid, delta, onRemove}) => {
   const [mediaData, setMediaData] = useState(false);
 
   useEffect(() => {
     if (!mediaData) {
+      // Fetch the media data. If the content is not json or the page fails, it will just bail.
       fetch(`/media/${mid}/edit?_format=json`)
         .then(response => response.json())
         .then(jsonData => {
           if (typeof jsonData.message !== 'undefined') {
             console.error(jsonData.message);
           }
+          // Always set the media data so that it will remove the loader tag.
           setMediaData(jsonData);
         });
     }
   }, []);
 
   if (!mediaData) {
-    return <Loader/>
+    return <PreviewContainer><Loader/></PreviewContainer>
   }
 
   return (
@@ -63,27 +74,30 @@ const MediaItem = ({mid, delta, onRemove}) => {
         onClick={() => onRemove(delta)}
         greyIcon
       />
-
-      {typeof mediaData.message === 'undefined' &&
-      <>
-        <div style={{background: "#ebebeb", padding: "0 20px"}}>
-          <PreviewImage
-            src={mediaData.thumbnail[0].url}
-            alt=""
-            role="presentation"
-          />
-        </div>
-        <div style={{padding: "5px"}}>
-          {mediaData.name[0].value}
-        </div>
-      </>
-      }
-
-      {typeof mediaData.message !== 'undefined' &&
-      // When the media entity doesn't exist or some communication error occurs.
-      <div style={{padding: '20px'}}>Unable to provide a preview of media.</div>
-      }
-
+      {/* Wrap the preview image in an error handler. */}
+      <ErrorBoundary errorMessage={<div style={{padding: '20px'}}>Unable to provide a preview of media.</div>}>
+          <PreviewImage mediaData={mediaData}/>
+      </ErrorBoundary>
     </PreviewContainer>
+  )
+}
+
+/**
+ * The preview image tag and name.
+ */
+const PreviewImage = ({mediaData}) => {
+  return (
+    <>
+      <div style={{background: "#ebebeb", padding: "0 20px"}}>
+        <PreviewImageTag
+          src={mediaData.thumbnail[0].url}
+          alt=""
+          role="presentation"
+        />
+      </div>
+      <div style={{padding: "5px"}}>
+        {mediaData.name[0].value}
+      </div>
+    </>
   )
 }
