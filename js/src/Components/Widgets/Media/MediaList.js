a51add2c66d112089c5070cb80601d4bfbf7bb41
Mike Decker <pookmish@gmail.com>
2021-02-23 10:59:04 -0800

    D8CORE-3380 Added sortable functionality for the media widget

--- js/src/Components/Widgets/Media/MediaList.js
+++ js/src/Components/Widgets/Media/MediaList.js
@@ -4,6 +4,9 @@ import {FlexDiv} from "../../Atoms/FlexDiv";
 import {XButton} from "../../Atoms/XButton";
 import {Loader} from "../../Atoms/Loader";
 import {ErrorBoundary} from "../../Atoms/ErrorBoundary";
+import {SortableContainer, SortableElement} from "react-sortable-hoc";
+
+const arrayMove = require('array-move');
 
 const PreviewContainer = styled.div`
   border: 1px solid #dbdbdb;
@@ -12,6 +15,7 @@ const PreviewContainer = styled.div`
   margin: 0 10px 10px 0;
   min-height: 100px;
   min-width: 100px;
+  z-index: 999;
 `;
 
 const PreviewImageTag = styled.img`
@@ -21,24 +25,47 @@ const PreviewImageTag = styled.img`
   max-width: 100%;
 `;
 
+const SortableItem = SortableElement(({item, delta, onRemove}) => (
+  <MediaItem
+    mid={item.target_id}
+    onRemove={onRemove}
+    delta={delta}
+  />
+));
+
+const SortableList = SortableContainer(({items, onRemoveItem}) => {
+  return (
+    <FlexDiv style={{flexWrap: 'wrap'}}>
+      {items.map((item, delta) => (
+        <SortableItem
+          key={item.target_uuid}
+          index={delta}
+          item={item}
+          delta={delta}
+          onRemove={onRemoveItem}
+        />
+      ))}
+    </FlexDiv>
+  );
+});
+
 /**
  * List of media items for media widget.
  */
 export const MediaList = ({selectedItems, onRemove, updateOrder}) => {
 
+  const onSortEnd = ({oldIndex, newIndex}) => {
+    updateOrder(arrayMove(selectedItems, oldIndex, newIndex));
+  }
+
   return (
-    <FlexDiv
-      style={{flexWrap: 'wrap'}}
-    >
-      {selectedItems.map((item, delta) =>
-        <MediaItem
-          key={`media-${delta}-${item.target_id}`}
-          mid={item.target_id}
-          delta={delta}
-          onRemove={onRemove}
-        />
-      )}
-    </FlexDiv>
+    <SortableList
+      axis="xy"
+      transitionDuration={0}
+      items={selectedItems}
+      onSortEnd={onSortEnd}
+      onRemoveItem={onRemove}
+    />
   )
 }
 
@@ -50,7 +77,8 @@ const MediaItem = ({mid, delta, onRemove}) => {
 
   useEffect(() => {
     if (!mediaData) {
-      // Fetch the media data. If the content is not json or the page fails, it will just bail.
+      // Fetch the media data. If the content is not json or the page fails, it
+      // will just bail.
       fetch(`/media/${mid}/edit?_format=json`)
         .then(response => response.json())
         .then(jsonData => {
@@ -75,7 +103,13 @@ const MediaItem = ({mid, delta, onRemove}) => {
         greyIcon
       />
       {/* Wrap the preview image in an error handler. */}
-      <ErrorBoundary errorMessage={<div style={{padding: '20px'}}>Unable to provide a preview of media.</div>}>
+      <ErrorBoundary
+        errorMessage={
+          <div style={{padding: '20px'}}>
+            Unable to provide a preview of media.
+          </div>
+        }
+      >
         <PreviewImage mediaData={mediaData}/>
       </ErrorBoundary>
     </PreviewContainer>
