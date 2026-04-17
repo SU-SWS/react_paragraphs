d11168f7d22e4fa2a6bc1004026b100ea731b0f4
Mike Decker <pookmish@stanford.edu>
2026-04-17 14:12:33 -0700

    fixup

--- tests/src/Kernel/Plugin/Field/ReactParagraphsFieldTestBase.php
+++ tests/src/Kernel/Plugin/Field/ReactParagraphsFieldTestBase.php
@@ -53,6 +53,7 @@ abstract class ReactParagraphsFieldTestBase extends KernelTestBase {
    */
   public function setup(): void {
     parent::setUp();
+    $this->container->get('module_installer')->install(['paragraphs']);
     $this->installEntitySchema('node');
     $this->installEntitySchema('user');
     $this->installEntitySchema('field_storage_config');
